ca094160e7828d04fef1150e42353bdb1dbe79c9
n0tst3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2023-03-26 14:08:56 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,15 +1,16 @@
 -- edit by n0tst3
 -- Pre-load the language
+local Config = Config
+local Languages = Languages
 local lang = Languages[Config.language]
 
--- @desc Handle /do command
 local function onDoCommand(source, args)
-    local text = "~b~* " .. lang.doPrefix .. table.concat(args, " ") .. " "
+    local text = table.concat(args, " ")
     TriggerClientEvent('3ddo:shareDisplay', -1, text, source)
 end
 
 local function onMeCommand(source, args)
-    local text = "~r~** " .. lang.mePrefix .. table.concat(args, " ") .. " **"
+    local text = table.concat(args, " ")
     TriggerClientEvent('3dme:shareDisplay', -1, text, source)
 end
 
