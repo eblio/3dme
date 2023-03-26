d75725b99e4295ed49236423bce328286edaa19a
n0tst3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2023-03-26 14:07:55 +0100

    Update client.lua
    simplified displayText

--- client.lua
+++ client.lua
@@ -1,22 +1,16 @@
     -- edit by n0tst3
-    local c = Config -- Pre-load the config
-    local lang = Languages[Config.language] -- Pre-load the language
-    local peds = {}
+    local Config = Config
+local Languages = Languages
+local c, lang = Config, Languages[Config.language]
 
--- Localization
-    local GetGameTimer = GetGameTimer
+local peds = {}
 
--- @desc Draw text in 3d
--- @param coords world coordinates to where you want to draw the text
--- @param text the text to display
-    local function draw3dText(coords, text)
+local function draw3dText(coords, text)
     local camCoords = GetGameplayCamCoord()
     local dist = #(coords - camCoords)
 
-    -- Experimental math to scale the text down
     local scale = 200 / (GetGameplayCamFov() * dist)
 
-    -- Format the text
     SetTextColour(c.color.r, c.color.g, c.color.b, c.color.a)
     SetTextScale(0.0, c.scale * scale)
     SetTextFont(c.font)
@@ -24,19 +18,14 @@
     SetTextDropShadow()
     SetTextCentre(true)
 
-    -- Diplay the text
     BeginTextCommandDisplayText("STRING")
     AddTextComponentSubstringPlayerName(text)
     SetDrawOrigin(coords, 0)
     EndTextCommandDisplayText(0.0, 0.0)
     ClearDrawOrigin()
-
 end
 
--- @desc Display the text above the head of a ped
--- @param ped the target ped
--- @param text the text to display
-    local function displayMeText(ped, text)
+local function displayText(ped, text, yOffset)
     local playerPed = PlayerPedId()
     local playerPos = GetEntityCoords(playerPed)
     local targetPos = GetEntityCoords(ped)
@@ -44,57 +33,25 @@ end
     local los = HasEntityClearLosToEntity(playerPed, ped, 17)
 
     if dist <= c.dist and los then
-        local exists = peds[ped] ~= nil
-
         peds[ped] = {
             time = GetGameTimer() + c.time,
-            text = text
+            text = text,
+            yOffset = yOffset
         }
 
-        if not exists then
-            local display = true
+        if not peds[ped].exists then
+            peds[ped].exists = true
 
-            while display do
-                Wait(0)
-                local pos = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.0, 0.7)
-                draw3dText(pos, peds[ped].text)
-                display = GetGameTimer() <= peds[ped].time
-            end
+            Citizen.CreateThread(function()
+                while GetGameTimer() <= peds[ped].time do
+                    local pos = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.0, peds[ped].yOffset)
+                    draw3dText(pos, peds[ped].text)
+                    Citizen.Wait(0)
+                end
 
-            peds[ped] = nil
+                peds[ped] = nil
+            end)
         end
-
-    end
-end
-
-    local function displayDoText(ped, text)
-    local playerPed = PlayerPedId()
-    local playerPos = GetEntityCoords(playerPed)
-    local targetPos = GetEntityCoords(ped)
-    local dist = #(playerPos - targetPos)
-    local los = HasEntityClearLosToEntity(playerPed, ped, 17)
-
-    if dist <= c.dist and los then
-        local exists = peds[ped] ~= nil
-
-        peds[ped] = {
-            time = GetGameTimer() + c.time,
-            text = text
-        }
-
-        if not exists then
-            local display = true
-
-            while display do
-                Wait(0)
-                local pos = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.0, 0.45)
-                draw3dText(pos, peds[ped].text)
-                display = GetGameTimer() <= peds[ped].time
-            end
-
-            peds[ped] = nil
-        end
-
     end
 end
 
@@ -102,7 +59,7 @@ local function onDoShareDisplay(text, target)
     local player = GetPlayerFromServerId(target)
     if player ~= -1 or target == GetPlayerServerId(PlayerId()) then
         local ped = GetPlayerPed(player)
-        displayDoText(ped, text)
+        displayText(ped, "~b~* " .. lang.doPrefix .. text .. " ", 0.45)
     end
 end
 
@@ -110,7 +67,7 @@ local function onMeShareDisplay(text, target)
     local player = GetPlayerFromServerId(target)
     if player ~= -1 or target == GetPlayerServerId(PlayerId()) then
         local ped = GetPlayerPed(player)
-        displayMeText(ped, text)
+        displayText(ped, "~r~** " .. lang.mePrefix .. text .. " **", 0.7)
     end
 end
 
